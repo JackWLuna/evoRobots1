5b6e0de1809064952585e7a5f148ca47c0009e0a
JackWLuna <56098857+JackWLuna@users.noreply.github.com>
2023-01-22 14:07:53 -0500

    added generate.py

--- simulate.py
+++ simulate.py
@@ -1,7 +1,10 @@
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 import time as t
 
 physicsClient = p.connect(p.GUI)
+
+pyrosim.Start_SDF("box.sdf")
 
 for i in range(1000):
     p.stepSimulation()
